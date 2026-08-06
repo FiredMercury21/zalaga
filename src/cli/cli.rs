5d63817f8bf21e6d35496aefea78961c2664c594
Mohammed Najl <mohammednajl@Ahmeds-MacBook-Air.local>
2026-08-06 22:29:35 +0400

    feat(cli): Add scope and type verifiers to CLI.

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -1,8 +1,6 @@
-use crate::cli::utils::*;
+use crate::diagnostics::*;
 use clap::{Parser, Subcommand, ValueEnum};
 use std::path::PathBuf;
-
-use crate::cli::utils::load_file;
 
 /// zgc: Compiler and toolchain for Zalaga.
 #[derive(Parser)]
@@ -26,18 +24,20 @@ enum Target {
 enum Pass {
     Scope,
     Flow,
-    Inits,
     Type,
 }
 
-impl ToString for Pass {
-    fn to_string(&self) -> String {
-        match self {
-            Pass::Scope => "scope".to_string(),
-            Pass::Flow => "flow".to_string(),
-            Pass::Inits => "inits".to_string(),
-            Pass::Type => "type".to_string(),
-        }
+impl std::fmt::Display for Pass {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}",
+            match self {
+                Pass::Scope => "scope".to_string(),
+                Pass::Flow => "flow".to_string(),
+                Pass::Type => "type".to_string(),
+            }
+        )
     }
 }
 
@@ -73,7 +73,7 @@ enum Commands {
     },
     Verify {
         /// File to verify
-        #[arg(short, long, default_value = "main.zg")]
+        #[arg(short = 'i', long, default_value = "main.zg")]
         file: PathBuf,
         /// Verification pass.
         #[arg(short, long, default_value_t = Pass::Type)]
@@ -85,6 +85,10 @@ pub fn run() {
     use super::utils::*;
     use Commands::*;
 
+    fn file_name(path: &PathBuf) -> &str {
+        path.to_str().expect("Bad file name.")
+    }
+
     match Cli::parse().command {
         Build {
             file,
@@ -92,21 +96,69 @@ pub fn run() {
             target,
             force,
         } => {
-            let file_str = load_file(&file).expect("Bad file.");
-            println!("{}", file_str);
+            let Some((fname, file_str, mut cache)) = load_pathbuf(&file) else {
+                return;
+            };
             let out_str = match target {
-                Target::Tokens => format!("{:#?}", targets::tokenize(&file_str)),
-                Target::AST => format!(
-                    "{:#?}",
-                    targets::build_ast(&file_str, file.to_str().expect("Bad Path"))
-                ),
+                Target::Tokens => {
+                    format!("{:#?}", targets::tokenize(&file_str, &PathBuf::from(fname)))
+                }
+                Target::AST => format!("{:#?}", {
+                    cache.insert_parsing(&(file.clone().into()));
+                    let ast = targets::build_ast(&file_str, &fname, &mut cache);
+                    cache.pop_parsing(&(file.clone().into()));
+                    ast
+                }),
                 Target::IR => todo!(),
                 Target::Asm => todo!(),
                 Target::ELF => todo!(),
             };
-            save_to_file(&output, &out_str, force);
+            if let Err(e) = save_to_file(&output, &out_str, force) {
+                eprintln!("Failed to save file, error: {e}");
+            };
         }
         Run { .. } => println!("Running..."),
-        Verify { .. } => println!("Verifying..."),
+        Verify { file, pass } => {
+            use crate::diagnostics::print_report;
+            use crate::semantics::*;
+
+            let Some((fname, file_str, mut cache)) = load_pathbuf(&file) else {
+                return;
+            };
+            cache.insert_parsing(&(file.clone().into()));
+            let parse_output = targets::build_ast(&file_str, &fname, &mut cache);
+            cache.pop_parsing(&(file.clone().into()));
+            let ast = match parse_output {
+                Ok(root) => root,
+                Err(e) => {
+                    print_report(e, &mut cache);
+                    return;
+                }
+            };
+
+            match pass {
+                Pass::Scope => {
+                    if let Err(e) = scope_checker::populate_scope(&ast) {
+                        print_report(e, &mut cache);
+                        return;
+                    };
+                }
+                Pass::Type => {
+                    let mut scope_table = match scope_checker::populate_scope(&ast) {
+                        Ok(table) => table,
+                        Err(e) => {
+                            print_report(e, &mut cache);
+                            return;
+                        }
+                    };
+                    if let Err(e) = type_checker::type_check(&ast, &mut scope_table) {
+                        print_report(e, &mut cache);
+                        return;
+                    }
+                }
+                Pass::Flow => todo!(),
+            }
+            println!("Pass verified.")
+        }
     }
 }
--- src/cli/utils.rs
+++ src/cli/utils.rs
@@ -1,10 +1,14 @@
+use crate::ast::ast_types::Path;
+use crate::diagnostics::PathCache;
 use std::fs;
 use std::path::PathBuf;
 
-pub fn ast_path_to_file(path: Vec<String>) -> Result<String, std::io::Error> {
-    let fpath_str = path.join("/") + ".zg";
+pub fn ast_path_to_file(path: &Path, cache: &mut PathCache) -> Result<String, std::io::Error> {
+    let fpath_str = path.0.join("/") + ".zg";
     let fpath = fpath_str.parse::<PathBuf>().expect("Bad path.");
-    load_file(&fpath)
+    let file_str = load_file(&fpath)?;
+    cache.insert_source(path, &file_str);
+    Ok(file_str)
 }
 
 pub fn load_file(path: &PathBuf) -> Result<String, std::io::Error> {
@@ -23,26 +27,47 @@ pub fn save_to_file(path: &PathBuf, file_str: &str, force: bool) -> Result<(), s
     fs::write(path, file_str)
 }
 
+pub fn load_pathbuf(file: &PathBuf) -> Option<(String, String, PathCache)> {
+    let text = match load_file(file) {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("error: could not read `{}`: {e}", file.display());
+            return None;
+        }
+    };
+    let mut cache = PathCache::new();
+    cache.insert_source(&file.clone().into(), &text);
+    Some((file.to_string_lossy().into_owned(), text, cache))
+}
+
 pub mod targets {
+    use super::*;
     use crate::ast::ast_types::*;
     use crate::ast::*;
 
-    pub fn tokenize(file_str: &str) -> Vec<Token> {
-        lexer::tokenize_code(file_str)
+    pub fn tokenize(file_str: &str, name: &PathBuf) -> Vec<Token> {
+        lexer::tokenize_code(file_str, name)
     }
 
-    pub fn build_ast(file_str: &str, name: &str) -> Result<Node, tree::ParseError> {
-        let tokens = lexer::tokenize_code(file_str);
-        tree::parse_file(tokens, name)
+    pub fn build_ast(
+        file_str: &str,
+        name: &str,
+        cache: &mut PathCache,
+    ) -> Result<Node, tree::ParseError> {
+        let tokens = lexer::tokenize_code(file_str, &PathBuf::from(name));
+        tree::parse_file(tokens, name, cache)
     }
 }
 
 pub mod verifiers {
+    use super::*;
+
     pub fn scope_check(
         file_str: &str,
         name: &str,
-    ) -> Result<(), crate::semantics::types::ScopeError> {
-        let ast = super::targets::build_ast(file_str, name).expect("Couldn't build AST");
+        cache: &mut PathCache,
+    ) -> Result<(), crate::semantics::sem_types::ScopeError> {
+        let ast = super::targets::build_ast(file_str, name, cache).expect("Couldn't build AST");
         crate::semantics::scope_checker::populate_scope(&ast)?;
         Ok(())
     }
